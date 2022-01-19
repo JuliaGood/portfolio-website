96d08d355ff6c75478724b6e992bc92ef28f3d72
JuliaGood <gudak.yuliya@gmail.com>
2022-01-19 20:17:38 +0200

    code refactoring

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const projectLinks = document.querySelectorAll(".project-link");
 const closeButtons = document.querySelectorAll(".modal-close-btn");
 
+const openPopup = (event) => {
+  event.preventDefault();
+  const popupName = event.currentTarget.getAttribute("data-popup-open");
+  document.querySelector(`.modal[data-popup="${popupName}"]`).style.display = "block";
+  document.querySelector('body').style.overflow = "hidden";
+}
+
+const closePopup = (event) => {
+  event.preventDefault();
+  event.currentTarget.closest(".modal").style.display = "none";
+  document.querySelector('body').style.overflow = "visible";
+}
+
 Array.from(projectLinks).forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    const popupName = link.getAttribute("data-popup-open");
-    document.querySelector(`.modal[data-popup="${popupName}"]`).style.display = "block";
-    document.querySelector('body').style.overflow = "hidden";
-  });
+  link.addEventListener("click", openPopup);
 });
 
 Array.from(closeButtons).forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    event.preventDefault();
-    btn.closest(".modal").style.display = "none";
-    document.querySelector('body').style.overflow = "visible";
-  });
+  btn.addEventListener("click", closePopup);
 });
