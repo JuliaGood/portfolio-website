e82b1236647ca14fbb91025941b4588f5d56691d
JuliaGood <gudak.yuliya@gmail.com>
2022-02-23 01:03:41 +0200

    final changes 2

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ const openPopup = (event) => {
   event.stopPropagation();
   const popupName = event.currentTarget.getAttribute("data-popup-open");
   document.querySelector(`.modal[data-popup="${popupName}"]`).classList.add("open");
-  document.querySelector("body").classList.add("popup-open");
+  document.querySelector("body").classList.add("no-scroll");
 }
 
 const closePopup = (event) => {
   event.preventDefault();
   event.currentTarget.closest(".modal").classList.remove("open");
-  document.querySelector("body").classList.remove("popup-open");
+  document.querySelector("body").classList.remove("no-scroll");
 }
 
 Array.from(projectLinks).forEach((link) => {
@@ -30,19 +30,25 @@ Array.from(closeButtons).forEach((btn) => {
 body.addEventListener("click", (e) => {
   //console.log(e.target);
 
-  if (body.classList.contains("popup-open") && e.target.classList.contains("modal")) {
+  if (body.classList.contains("no-scroll") && e.target.classList.contains("modal")) {
     document.querySelector(".modal.open").classList.remove("open");
-    document.querySelector("body").classList.remove("popup-open");
+    body.classList.remove("no-scroll");
   }
 });
 
 const toggleMenu = () => {
   burgerBtn.classList.toggle("open");
   mobileNav.classList.toggle("open");
+
+  if (mobileNav.classList.contains("open")) {
+    body.classList.add("no-scroll");
+  } else {
+    body.classList.remove("no-scroll");
+  }
 }
 
 burgerBtn.addEventListener("click", () => toggleMenu());
 
 Array.from(mobileNavLis).forEach((li) => {
-  li.addEventListener("click", () => toggleMenu())
+  li.addEventListener("click", () => toggleMenu());
 });
